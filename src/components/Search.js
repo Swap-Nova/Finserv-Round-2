5d18b4193608f8da5e6f35cb28f1a2c9cda6b3e9
FirstThinker_Swapnil <swapnilsinha81@gmail.com>
2023-05-21 11:11:17 +0530

    New functionality : Checkbox

---
+++ src/components/Search.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
+
+function Search() {
+    const [nameList, setNameList] = useState([]);
+    const [search, setSearch] = useState('');
+    const [selectedSkills, setSelectedSkills] = useState([]);
+
+    useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json')
+        .then((response) => { setNameList(response.data.employees) })
+      // .then((response)=>console.log(response))
+    }, []);
+
+    const handleSkillChange = (event) => {
+            const { value, checked } = event.target;
+            setSelectedSkills(prevSkills => {
+            if (checked) {
+                return [...prevSkills, value];
+            } else {
+                return prevSkills.filter(skill => skill !== value);
+            }
+        });
+    };
+
+    const filterEmployeesBySkills = (employee) => {
+        if (selectedSkills.length === 0) {
+            return true;
+        } else {
+            return selectedSkills.every(skill => employee.skills.includes(skill));
+        }
+    };
+
+    return (
+        <div className="top-container">
+            <h1>Employees List</h1>
+            <input type="text" placeholder="Type to search" onChange={(e) => setSearch(e.target.value)} />
+        {/* eslint-disable-next-line array-callback-return */}
+        {nameList.filter((item) => {
+            if (search === "") {
+            return item;
+            }
+            else if (item.name && item.name.toLowerCase().includes(search.toLowerCase())) {
+            return item;
+            }
+            else if (item.designation && item.designation.toLowerCase().includes(search.toLowerCase())) {
+            return item;
+            }
+            return null;
+        })
+            .filter(filterEmployeesBySkills)
+            .map((item) => {
+            return (
+                <ul key={item.id}>
+                <p>{item.name}</p>
+                <p>{item.designation}</p>
+                <p>{item.skills}</p>
+                </ul>
+            );
+            })}
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Skills
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                <Form>
+                    <div className="mb-3">
+                    {['SQL', 'JavaScript', 'Python', 'HTML', 'CSS', 'Photoshop', 'Manual Testing', 'Java'].map((skill) => (
+                        <Form.Check
+                        key={skill}
+                        type="checkbox"
+                        id={skill}
+                        label={skill}
+                        value={skill}
+                        checked={selectedSkills.includes(skill)}
+                        onChange={handleSkillChange}
+                        />
+                    ))}
+                    </div>
+                </Form>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
+}
+
+export default Search;
